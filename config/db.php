9bf9db0b7c13b7f8783adee0ae1104303bfcbe7b
Mikhail <mvvershinin@gmail.com>
2016-11-24 14:20:32 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=umka',
     'username' => 'umkadb',
-    'password' => 'FDHnji532n%$hgf',
+    'password' => '',
     'charset' => 'utf8',
 ];
